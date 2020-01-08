98088e5fe8a9d5dde805834aea3cd5528e09f89a
Alvin Yao <ayao00@stuy.edu>
2020-01-08 10:43:24 -0500

    added struct player

--- play.c
+++ play.c
@@ -11,12 +11,44 @@
 #include <fcntl.h>
 #include <ctype.h>
 
+struct player{
+  char name[100];
+  int pastgames;
+  int victories;
+  int losses;
+  boolean online;
+}
+
+struct player makePlayer(char * name){
+  struct ptudent newPlayer;
+  strcpy(newPlayer.name, name);
+  newPlayer.pastgames = 0;
+  newPlayer.victories = 0;
+  newPlayer.losses = 0;
+  newPlayer.online = true;
+  return newPlayer;
+}
+
 void findPlayer(char * name){
 
 }
 
 void makePlayer(char * name){
 
+}
+
+void printPlayer(struct Player user){
+  printf("Player info:\n");
+  printf("Name:\t%s\n", user.name);
+  printf("# of past games:\t%d\n\n", user.pastgames);
+  printf("# of victories:\t%d\n\n", user.victories);
+  printf("# of losses:\t%d\n\n", user.losses);
+  printf("Connected!\n");
+}
+
+void editStudent(struct Student *newStudent, char* newName, int newGrade) {
+  strcpy(newStudent->name, newName);
+  newStudent->grade = newGrade;
 }
 
 int main(int argc, char * argv[]){
