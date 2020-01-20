39828585f7956321204ff6d4e7dfb10669f446e3
Alvin Yao <ayao00@stuy.edu>
2020-01-20 15:23:47 -0500

    trying somethign new

--- networking/client.c
+++ networking/client.c
@@ -1,31 +1,39 @@
 #include "networking.h"
 
-int main(int argc, char **argv) {
-
-  int server_socket;
+struct input{
   char x[BUFFER_SIZE];
   char y[BUFFER_SIZE];
   char choice[BUFFER_SIZE];
-  char buffer[BUFFER_SIZE * 3];
+};
+int main(int argc, char **argv) {
 
+  int server_socket;
+  char buffer[BUFFER_SIZE];
+  // char x[BUFFER_SIZE];
+  // char y[BUFFER_SIZE];
+  // char choice[BUFFER_SIZE];
+  // char buffer[BUFFER_SIZE * 3];
+
+  struct input current;
   if (argc == 2)
     server_socket = client_setup( argv[1]);
   else
     server_socket = client_setup( TEST_IP );
 
   while (1) {
+
     printf("Enter an x-coor: ");
-    fgets(x, sizeof(buffer), stdin);
+    fgets(current.x, sizeof(buffer), stdin);
 
     printf("Enter a y-coor: ");
-    fgets(y, sizeof(buffer), stdin);
+    fgets(current.y, sizeof(buffer), stdin);
 
     printf("Uncover or flag? (f/u): ");
-    fgets(choice, sizeof(buffer), stdin);
-    strcpy(buffer, x);
-    strcat(buffer, y);
-    strcat(buffer, choice);
-    *strchr(buffer,'\n') = 0;
+    fgets(current.choice, sizeof(buffer), stdin);
+
+    strcpy(buffer, current.x);
+    strcat(buffer, current.y);
+    strcat(buffer, current.choice);
     write(server_socket, buffer, sizeof(buffer));
     read(server_socket, buffer, sizeof(buffer));
     printf("received: [%s]\n", buffer);
